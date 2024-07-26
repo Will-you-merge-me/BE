4aa15ec6481e89dc3b8263eccd57d43a8d8755c1
Doritosch <als000825@naver.com>
2024-07-26 16:37:35 +0900

    add mvc pattern

---
+++ UserServer/src/main/java/com/springboot/userserver/data/dao/UserDAO.java
@@ -0,0 +1,4 @@
+package com.springboot.userserver.data.dao;
+
+public interface UserDAO {
+}
---
+++ UserServer/src/main/java/com/springboot/userserver/data/dao/impl/UserDAOImpl.java
@@ -0,0 +1,9 @@
+package com.springboot.userserver.data.dao.impl;
+
+import com.springboot.userserver.data.dao.UserDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDAOImpl implements UserDAO {
+
+}
