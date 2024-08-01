a2a86b188ce7b27358df86b8fb01b81bf24ec305
dongri <l0o0lv@github.com>
2024-08-02 01:01:20 +0900

    :white_check_mark: 이미지 구현

--- ProductServer/src/main/java/com/example/productserver/Dto/MenuDto.java
+++ ProductServer/src/main/java/com/example/productserver/Dto/MenuDto.java
@@ -1,15 +1,23 @@
 package com.example.productserver.Dto;
 
+import com.example.productserver.Entity.MenuEntity;
 import com.example.productserver.Entity.ProductEntity;
+import lombok.Builder;
 
-
+@Builder
 public class MenuDto {
 
-    private ProductEntity productEntity;
+    private Long productId;
 
     private String title; //이름
     private String content; //내용
     private long price; //가격
     private String image; //사진
     private long hour; //강의 시간
+
+    public static MenuEntity dtoToEntity(MenuDto menuDto) {
+        return MenuEntity.builder()
+                .
+                build();
+    }
 }
