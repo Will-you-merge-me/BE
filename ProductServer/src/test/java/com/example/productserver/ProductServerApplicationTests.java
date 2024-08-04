4cf7f3eed8451f8145fa42ccb3acb4aa9f072191
dongri <l0o0lv@github.com>
2024-08-04 15:58:04 +0900

    :white_check_mark: eureka update

--- ProductServer/src/test/java/com/example/productserver/ProductServerApplicationTests.java
+++ ProductServer/src/test/java/com/example/productserver/ProductServerApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.productserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootTest
 class ProductServerApplicationTests {
 
