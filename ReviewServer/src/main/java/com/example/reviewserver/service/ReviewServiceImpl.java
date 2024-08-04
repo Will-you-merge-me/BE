f532c4923c217a22a4477e86a9ca666293e05fd8
dongri <l0o0lv@github.com>
2024-08-04 17:01:42 +0900

    리뷰 사진, 수정 추가

--- ReviewServer/src/main/java/com/example/reviewserver/service/ReviewServiceImpl.java
+++ ReviewServer/src/main/java/com/example/reviewserver/service/ReviewServiceImpl.java
@@ -54,7 +54,7 @@ public class ReviewServiceImpl implements ReviewService{
     }
 
     /**
-     * 상품 리뷰 조회
+     * 상품 리뷰 조회d
      */
     public List<ReviewDto> findProductReviews(Long productId){
         List<Review> reviews = reviewRepository.findByProductId(productId);
