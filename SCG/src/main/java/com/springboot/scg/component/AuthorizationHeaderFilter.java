019c9c1852416dbebfb847c13416c90ea84df175
Doritosch <als000825@naver.com>
2024-08-05 17:41:59 +0900

    change jwt version

--- SCG/src/main/java/com/springboot/scg/component/AuthorizationHeaderFilter.java
+++ SCG/src/main/java/com/springboot/scg/component/AuthorizationHeaderFilter.java
@@ -1,7 +1,8 @@
 package com.springboot.scg.component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,82 +11,73 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
 import java.util.Base64;
+import java.util.Date;
 
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
     @Value("${secret.jwt.token.key}")
-    String secretkey;
-
-
+    String secretKey;
     private static final Logger logger = LoggerFactory.getLogger(AuthorizationHeaderFilter.class);
 
+    public AuthorizationHeaderFilter(){
+        super(Config.class);
+    }
     @PostConstruct
     protected void init(){
         logger.info("JwtTokenProvider : 초기화를 완료했습니다.(init)");
-        secretkey = Base64.getEncoder().encodeToString(secretkey.getBytes());
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
 
-    public AuthorizationHeaderFilter() {
-        super(Config.class);
+
+    public static class Config{
+        private String role;
+
+        public void setRequiredRole(String role){this.role = role;;}
     }
 
-    public static class Config {
 
+    private boolean isJwtValid(String jwt){ // JWT 유효성 검사
+        logger.info("JWT 유효성 검사");
+
+        try{
+            Jws<Claims> claims = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(jwt);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch(Exception e){
+            logger.error("JWT 유효성 검사 실패 = {} ",e.getMessage());
+            return false;
+        }
     }
+
 
     @Override
     public GatewayFilter apply(Config config) {
-        GatewayFilter filter = (exchange, chain) -> {
+        return  (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
+            logger.info("요청한 uri :" + request.getURI());
 
-            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                return onError(exchange, "No Authorization header", HttpStatus.UNAUTHORIZED);
-            }
-
+            //헤더에서 JWT 토큰 추출
             String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-            String jwt = authorizationHeader.replaceFirst("Bearer ", "");
+            String jwt = authorizationHeader.replace("Bearer ","");
 
-            if(!isJwtValid(jwt)) {
-                return onError(exchange, "JWT Token is not valid", HttpStatus.UNAUTHORIZED);
+            //JWT 토큰 유효성 검사
+            if(!isJwtValid(jwt)){
+                return onError(exchange,"JWT 토큰이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED);
             }
-
+            logger.info("JWT 토큰이 유효합니다.");
+            //JWT 토큰 유효하면 다음 필터로 요청 전달
             return chain.filter(exchange);
         };
-
-        return filter;
     }
 
-    private boolean isJwtValid(String jwt) {
-        boolean returnValue = true;
-
-        String subject = null;
-
-        try {
-            subject = Jwts.parserBuilder()
-                    .setSigningKey(secretkey)
-                    .build()
-                    .parseClaimsJws(jwt)
-                    .getBody()
-                    .getSubject();
-        } catch (Exception e) {
-            returnValue = false;
-        }
-
-        if(subject == null || subject.isEmpty()) {
-            returnValue = false;
-        }
-
-        return returnValue;
-    }
     private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(httpStatus);
@@ -93,4 +85,5 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         logger.error(err);
         return response.setComplete();
     }
+
 }
