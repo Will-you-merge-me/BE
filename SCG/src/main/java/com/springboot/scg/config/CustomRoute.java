81ae21c27069a36e0fc5449c907552485d64c014
dongri <l0o0lv@github.com>
2024-08-05 17:20:54 +0900

    route update

--- SCG/src/main/java/com/springboot/scg/config/CustomRoute.java
+++ SCG/src/main/java/com/springboot/scg/config/CustomRoute.java
@@ -1,7 +1,6 @@
 package com.springboot.scg.config;
 
 import com.springboot.scg.component.AuthorizationHeaderFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -18,21 +17,27 @@ public class CustomRoute {
     public RouteLocator ms1Route(RouteLocatorBuilder builder) {
 
         return builder.routes()
-                .route("user-server", r -> r.path("/user/signup")
-                        .uri("lb://user-server"))
-                .route("user-server", r -> r.path("/user/signup/trainer")
-                        .uri("lb://user-server"))
-                .route("user-server", r -> r.path("/user/signin")
-                        .uri("lb://user-server"))
                 .route("user-server", r -> r.path("/user/**")
+                        .uri("lb://user-server"))
+                .route("user-server", r -> r.path("/user/patch/{userId}")
                         .filters(f -> f.filter(jwtFilter.apply(new AuthorizationHeaderFilter.Config())))
                         .uri("lb://user-server"))
+                .route("user-server", r -> r.path("/user/delete/{userId}")
+                        .filters(f -> f.filter(jwtFilter.apply(new AuthorizationHeaderFilter.Config())))
+                        .uri("lb://user-server"))
+                .route("user-server", r -> r.path("/user/profile/{userId}")
+                        .filters(f -> f.filter(jwtFilter.apply(new AuthorizationHeaderFilter.Config())))
+                        .uri("lb://user-server"))
+
 
                 .route("product-server", r -> r.path("/product/**")
                         .uri("lb://product-server"))
 
                 .route("review-server", r -> r.path("/review/**")
                         .uri("lb://review-server"))
+
+                .route("purchase-server", r-> r.path("/purchase/**")
+                        .uri("lb://purchase-server"))
 
                 .build();
     }
