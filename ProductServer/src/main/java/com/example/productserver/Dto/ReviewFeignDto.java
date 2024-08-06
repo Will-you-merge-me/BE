e0c6377c017fe09ac272aa98214d540b721e6a34
dongri <l0o0lv@github.com>
2024-08-06 19:41:56 +0900

    reviewFeign 수정

--- ProductServer/src/main/java/com/example/productserver/Dto/ReviewFeignDto.java
+++ ProductServer/src/main/java/com/example/productserver/Dto/ReviewFeignDto.java
@@ -2,7 +2,11 @@ package com.example.productserver.Dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@NoArgsConstructor
 @Getter
 @AllArgsConstructor
 public class ReviewFeignDto {
